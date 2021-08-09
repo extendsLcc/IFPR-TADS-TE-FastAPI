4e61b034eb02ebddc1870f39cce436faf4f71100
extendslcc <lucas.lcc@hotmail.com>
2021-08-09 19:40:26 -0300

    feat: add post item route example

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    id: int
+    descricao: str
+    valor: float
+
+
 app = FastAPI()
 
 
@@ -13,3 +22,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/item")
+def add_item(novo_item: Item):
+    return novo_item
