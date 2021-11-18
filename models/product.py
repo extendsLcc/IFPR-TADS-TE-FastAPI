72e01aa5e03ca61413d402c2272b38b4235f1d03
extendslcc <lucas.lcc@hotmail.com>
2021-11-17 22:03:47 -0300

    chore: created product model

---
+++ models/product.py
@@ -0,0 +1,13 @@
+import ormar
+from config import database, metadata
+
+
+class Product(ormar.Model):
+    class Meta:
+        metadata = metadata
+        database = database
+
+    id: int = ormar.Integer(primary_key=True)
+    name: str = ormar.String(max_length=255)
+    price: float = ormar.Float()
+    stock: int = ormar.Integer()
